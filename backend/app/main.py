3f122a946a4d117f78c96deebb4d7938981b539c
shakthiswaran-M <sundar.k@netkathir.com>
2026-09-02 15:47:52 +0530

    tts error check

--- backend/app/main.py
+++ backend/app/main.py
@@ -11,6 +11,7 @@ app.add_middleware(
     allow_origins=[
         "http://localhost:5173",
         "https://netkathirbot.vercel.app",
+        "http://127.0.0.1:5173"
     ],
     allow_credentials=True,
     allow_methods=["*"],
